0eca477279e62a19c11588f578ea9e6b497985b2
hifertech <user.hifertech1@gmail.com>
2025-03-13 11:04:24 +0100

    price table

--- app/SearchResult/page.tsx
+++ app/SearchResult/page.tsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import SearchFlightResult from '../components/SearchPage/SearchFlightResult';
 import Filters from '../components/SearchPage/Filters';
+import PriceTable from '../components/SearchPage/PriceTable';
 
 export default function SearchResult() {
   const searchParams = useSearchParams();
@@ -31,8 +32,10 @@ export default function SearchResult() {
           </div>
           <div className="lg:col-span-3 relative z-10">
             <div className="bg-white/90 backdrop-blur-sm rounded-lg shadow-lg p-6">
-              <h2 className="text-xl font-semibold mb-4 text-gray-800">Available Flights</h2>
-              <p className="text-gray-600">Flight results will be displayed here</p>
+              <div className="mb-6">
+                <PriceTable />
+              </div>
+              
             </div>
           </div>
         </div>
